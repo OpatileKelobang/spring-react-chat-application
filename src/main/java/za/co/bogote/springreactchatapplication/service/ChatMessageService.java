0676005c13bac7efab5b28ab01940a57837059f3
OpatileKelobang <kelobang.opatile@gmail.com>
2020-10-19 20:51:35 +0200

    Chat Message Repository

--- src/main/java/za/co/bogote/springreactchatapplication/service/ChatMessageService.java
+++ src/main/java/za/co/bogote/springreactchatapplication/service/ChatMessageService.java
@@ -2,8 +2,16 @@ package za.co.bogote.springreactchatapplication.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
+import za.co.bogote.springreactchatapplication.exception.ResourceNotFoundException;
+import za.co.bogote.springreactchatapplication.model.ChatMessage;
+import za.co.bogote.springreactchatapplication.model.MessageStatus;
 import za.co.bogote.springreactchatapplication.repository.ChatMessageRepository;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ChatMessageService {
@@ -14,4 +22,50 @@ public class ChatMessageService {
     private ChatRoomService chatRoomService;
     @Autowired
     private MongoOperations mongoOperations;
+
+    public ChatMessage save(ChatMessage chatMessage) {
+        chatMessage.setStatus(MessageStatus.RECEIVED);
+        repository.save(chatMessage);
+        return chatMessage;
+    }
+
+    public long countNewMessages(String senderId, String recipientId){
+        return repository.countBySenderIdAndRecipientIdAndStatus(
+                senderId, recipientId, MessageStatus.RECEIVED
+        );
+    }
+
+
+    public List<ChatMessage> findChatMessages(String senderId, String recipientId) {
+        var chatId = chatRoomService.getChatId(senderId, recipientId, false);
+        var messages = chatId.map(cId -> repository.findByChatId(cId)).orElse(new ArrayList<>());
+
+        if(messages.size() > 0) {
+            updateStatuses(senderId, recipientId, MessageStatus.DELIVERED);
+        }
+
+        return messages;
+    }
+
+    public ChatMessage findById(String id){
+        return repository
+                .findById(id)
+                .map(chatMessage -> {
+                    chatMessage.setStatus(MessageStatus.DELIVERED);
+                    return repository.save(chatMessage);
+                })
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Can't find message (" + id + ")"));
+    }
+
+    private void updateStatuses(String senderId, String recipientId, MessageStatus status) {
+        Query query = new Query(
+                Criteria
+                .where("senderId").is(senderId)
+                .and("recipientId").is(recipientId));
+        Update update = Update.update("status", status);
+        mongoOperations.updateMulti(query, update, ChatMessage.class);
+    }
+
+
 }
