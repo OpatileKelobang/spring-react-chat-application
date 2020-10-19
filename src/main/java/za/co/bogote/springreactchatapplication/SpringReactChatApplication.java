50dd11a4ee3d06275238bd33c398a27ea4c6a1d0
OpatileKelobang <kelobang.opatile@gmail.com>
2020-10-19 18:33:17 +0200

    Initial commit

---
+++ src/main/java/za/co/bogote/springreactchatapplication/SpringReactChatApplication.java
@@ -0,0 +1,13 @@
+package za.co.bogote.springreactchatapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReactChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/za/co/bogote/springreactchatapplication/SpringReactChatApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.bogote.springreactchatapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
